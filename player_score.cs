f02ca25730e4a3d8b9899d5feaa1f9371dd5929b
RachelMorton102 <40838050+RachelMorton102@users.noreply.github.com>
2018-07-11 11:59:58 -0400

    Add files via upload

---
+++ player_score.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class player_score : MonoBehaviour {
+
+    private GameObject[] pickups;
+
+    private int pickupCount;
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private GameObject YouWinPopUp;
+
+    [SerializeField]
+    private Text YouWinPopUpText;
+
+
+    private void Start()
+    {
+        Time.timeScale=1;
+        pickups = GameObject.FindGameObjectsWithTag("pickups");
+        pickupCount = pickups.Length;
+        scoreText.text = pickupCount.ToString() + "PICKUPS LEFT";
+        YouWinPopUp.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (pickupCount==0)
+        {
+            if (YouWinPopUp.activeInHierarchy == false)
+            {
+                Time.timeScale = 0;
+                YouWinPopUp.SetActive(true);
+                YouWinPopUpText.text = "You Win";
+                Debug.Log("you win");
+
+            }
+           
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {if(collider.tag=="pickups")
+        {
+            pickupCount--;
+            scoreText.text = pickupCount.ToString() + "PICKUPS LEFT";
+            GameObject.Destroy(collider.gameObject);
+            print("picked up");
+        }
+       
+    }
+}
